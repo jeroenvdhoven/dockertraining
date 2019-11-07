439be55c6b4751f9b57c4a3b9902498599f1094a
jeroenvdhoven <jeroenhoven@gmail.com>
2019-11-07 15:55:17 +0100

    Make base branch for starting training

--- gunicorn_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Default host will work well for testing without Docker.
-# However, use 0.0.0.0 with Docker. 127.0.0.1 is not open to the world,
-# which means you will not be able to access it on the Docker image from outside.
-host=${host:-"127.0.0.1"}
-
-gunicorn main.server.server:app -b $host:5000
