d9a62179e21be9683d3aa5879964e0ee2f24c4dd
jeroenvdhoven <jeroenhoven@gmail.com>
2020-01-03 15:44:58 +0100

    Update server code to serve multiple models

--- main/server/server.py
+++ main/server/server.py
@@ -4,24 +4,43 @@ import joblib
 import pandas as pd
 from flask import Flask
 
-from main import output_folder, model_file
+from main import output_folder
+from typing import List
+import json
+
+
+def create_model_paths(files: List[str]):
+    output_folder_files = map(
+        lambda f: os.path.join(output_folder, f),
+        files
+    )
+    return [
+        file if os.path.isfile(file) else os.path.join("static", file)
+        for file in output_folder_files
+    ]
+
 
 # Start an application
 app = Flask(__name__)
 
-# Read the pipeline
-target_file = os.path.join(output_folder, model_file)
-if not os.path.isfile(target_file):
-    target_file = os.path.join("static", model_file)
+# Get the paths of the models to be used
+original_files = os.listdir(output_folder)
+target_files = create_model_paths(original_files)
 
-pipeline = joblib.load(target_file)
+# Read the pipelines
+pipelines = {
+    original_file: joblib.load(target_file)
+    for target_file, original_file in zip(target_files, original_files)
+}
 
 
-# Make and endpoint that accepts one variable `data` as input
-@app.route('/<string:data>')
-def predict(data: str):
+@app.route('/<string:model>/<string:data>')
+def predict(model: str, data: str):
     converted_data = pd.read_json(data)
-    return str(pipeline.transform(converted_data))
+    return json.dumps({
+        "model": model,
+        "predictions": pipelines[model].transform(converted_data).tolist()
+    })
 
 
 # Run the app only if this file is run.
