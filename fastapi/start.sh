e63e59605546ff206b5ab17d128eeeccfacf082c
jeroenvdhoven <jeroenhoven@gmail.com>
2020-04-22 13:26:00 +0200

    Add basic docker image builder for fastapi

---
+++ fastapi/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Default host will work well for testing without Docker.
+# However, use 0.0.0.0 with Docker. 127.0.0.1 is not open to the world,
+# which means you will not be able to access it on the Docker image from outside.
+default_host="127.0.0.1"
+host=${host:-$default_host}
+
+echo $host
+uvicorn main.fastapi-server.server:app --host $host
